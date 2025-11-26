01a5cd8042bf7f7a202316d836a1684d65623272
Igor Ryan <igorsonic56@gmail.com>
2025-11-26 01:05:28 -0300

    Instalação do bcryptjs, atualização do arquivo .prisma, adicionado campo para senha no modelo de participante, autualização do controller de auteticação para comportar lógica de registro e login

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,9 +1,12 @@
 import express from 'express';
-import { login } from '../controllers/authController.js';
+import { login, cadastrarSenha } from '../controllers/authController.js';
 
 const router = express.Router();
 
-// Rota: POST /api/v1/auth/login (lembrar disso)
+// Rota para quem já tem senha: POST /api/v1/auth/login
 router.post('/login', login);
+
+// Rota para criar a senha (primeiro acesso): POST /api/v1/auth/register
+router.post('/register', cadastrarSenha);
 
 export default router;
