e73cb136e81e931f1250814dd36d8c21c60376e6
Davi Cruz <cruzdavi001@gmail.com>
2025-11-27 17:20:36 -0300

    Endpoint de listar todos usuarios para teste do registro de presenca

--- src/controllers/usuariosController.js
+++ src/controllers/usuariosController.js
@@ -3,10 +3,29 @@ import prisma from '../lib/prisma.js';
 const usuariosController = {
   async create(req, res) {
     const { nome, email } = req.body;
-    const usuarios = await prisma.usuario.create({
+    const usuarios = await prisma.participante.create({
       data: { nome, email },
     });
     res.json(usuarios);
+  },
+
+  // Listar todos os participantes
+  async listar(req, res) {
+    try {
+      const participantes = await prisma.participante.findMany({
+        select: {
+          id: true,
+          nome: true,
+          email: true,
+          even3Id: true,
+          foto: true
+        }
+      });
+      res.status(200).json(participantes);
+    } catch (error) {
+      console.error("Erro ao listar participantes:", error);
+      res.status(500).json({ error: "Erro ao listar participantes" });
+    }
   },
 };
 
