ea6cce9f251fe9272caf7a5b34d0d69bfb7ccf4e
yurepires <contato.yurepires@gmail.com>
2025-12-02 14:01:47 -0300

    feat: enpoint para retornar ao participante os quizzes respondidos, com pontuação, e os pendentes

--- src/routes/quizRoutes.js
+++ src/routes/quizRoutes.js
@@ -1,8 +1,9 @@
 import express from 'express'
-import { listarTodos, listarLiberados, buscarPorId, responder, buscarPorIdParticipante} from '../controllers/quizController.js'
+import { listarTodos, listarLiberados, buscarPorId, responder, buscarPorIdParticipante, listarComStatus} from '../controllers/quizController.js'
 
 const router = express.Router()
 
+router.get('/participante/status', listarComStatus) // GET http://localhost:5000/api/quizzes/participante/status
 router.get('/participante/:id', buscarPorIdParticipante) // GET http://localhost:5000/api/quizzes/participante/{ID}
 router.get('/liberados', listarLiberados)      // GET http://localhost:5000/api/quizzes/liberados
 router.post('/responder/:id', responder)       // POST http://localhost:5000/api/quizzes/responder/{ID}
